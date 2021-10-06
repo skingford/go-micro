8a10127d33fc58665e987dc576a485cc4d505fff
SteveKingFord <332312568@qq.com>
2021-10-06 21:07:29 +0800

    feat: proto gen go grpc gateway

--- server/genProto.sh
+++ server/genProto.sh
@@ -1,7 +1,7 @@
 ###
  # @Author: kingford
  # @Date: 2021-10-06 19:56:04
- # @LastEditTime: 2021-10-06 20:08:37
+ # @LastEditTime: 2021-10-06 21:06:24
 ### 
 function genProto {
     DOMAIN=$1
@@ -10,7 +10,9 @@ function genProto {
     GO_OUT_PATH=./${DOMAIN}/api/gen/v1
     mkdir -p $GO_OUT_PATH
 
-    protoc -I=$PROTO_PATH --go_out=plugins=grpc,paths=source_relative:$GO_OUT_PATH ${DOMAIN}.proto
+    protoc -I=$PROTO_PATH --go_out=$GO_OUT_PATH --go_opt=paths=source_relative \
+    --go-grpc_out=$GO_OUT_PATH --go-grpc_opt=paths=source_relative \
+    ${DOMAIN}.proto
 
     if [ $SKIP_GATEWAY ]; then
         return
@@ -22,7 +24,7 @@ function genProto {
     PBTS_OUT_DIR=../wx/src/service/proto_gen/${DOMAIN}
     mkdir -p $PBTS_OUT_DIR
     $PBTS_BIN_DIR/pbjs -t static -w es6 $PROTO_PATH/${DOMAIN}.proto --no-create --no-encode --no-decode --no-verify --no-delimited --force-number -o $PBTS_OUT_DIR/${DOMAIN}_pb_tmp.js
-    echo 'import * as $protobuf from "protobufjs";\n' > $PBTS_OUT_DIR/${DOMAIN}_pb.js
+    echo 'import * as $protobuf from "protobufjs";' > $PBTS_OUT_DIR/${DOMAIN}_pb.js
     cat $PBTS_OUT_DIR/${DOMAIN}_pb_tmp.js >> $PBTS_OUT_DIR/${DOMAIN}_pb.js
     rm $PBTS_OUT_DIR/${DOMAIN}_pb_tmp.js
     $PBTS_BIN_DIR/pbts -o $PBTS_OUT_DIR/${DOMAIN}_pb.d.ts $PBTS_OUT_DIR/${DOMAIN}_pb.js
