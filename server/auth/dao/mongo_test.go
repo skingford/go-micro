defdc1d1f65233000b3d18f4b2fe2a8ba3756068
SteveKingFord <332312568@qq.com>
2022-01-15 00:00:08 +0800

    feat: 驱动测试

--- server/auth/dao/mongo_test.go
+++ server/auth/dao/mongo_test.go
@@ -2,7 +2,11 @@ package dao
 
 import (
 	"context"
+	"go-micro/shared/id"
+	mgutil "go-micro/shared/mongo"
+	"go-micro/shared/mongo/objid"
 	mongotesting "go-micro/shared/mongo/testing"
+	"go.mongodb.org/mongo-driver/bson"
 	"os"
 	"testing"
 )
@@ -16,17 +20,56 @@ func TestMongo_ResolveAccountID(t *testing.T) {
 
 	m := NewMongo(mc.Database("coolcar"))
 
-	id, err := m.ResolveAccountID(c, "123")
-
+	_, err = m.col.InsertMany(c, []interface{}{
+		bson.M{
+			mgutil.IDFieldName: objid.MustFromID(id.AccountID("5f7c245ab0361e00ffb9fd6f")),
+			openIDField:        "openid_1",
+		},
+		bson.M{
+			mgutil.IDFieldName: objid.MustFromID(id.AccountID("5f7c245ab0361e00ffb9fd70")),
+			openIDField:        "openid_2",
+		},
+	})
 	if err != nil {
-		t.Errorf("failed to resolve account id for 123:%v", err)
-	} else {
-		want := "61ddb902326cb9515740e088"
-		if id != want {
-			t.Errorf("resolve account id: want:%q,got:%q", want, id)
-		}
+		t.Fatalf("cannot insert initial values: %v", err)
 	}
 
+	mgutil.NewObjIDWithValue(id.AccountID("5f7c245ab0361e00ffb9fd71"))
+
+	cases := []struct {
+		name   string
+		openID string
+		want   string
+	}{
+		{
+			name:   "existing_user",
+			openID: "openid_1",
+			want:   "5f7c245ab0361e00ffb9fd6f",
+		},
+		{
+			name:   "another_existing_user",
+			openID: "openid_2",
+			want:   "5f7c245ab0361e00ffb9fd70",
+		},
+		{
+			name:   "new_user",
+			openID: "openid_3",
+			want:   "5f7c245ab0361e00ffb9fd71",
+		},
+	}
+
+	for _, cc := range cases {
+		t.Run(cc.name, func(t *testing.T) {
+			aid, err := m.ResolveAccountID(
+				context.Background(), cc.openID)
+			if err != nil {
+				t.Errorf("faild resolve account id for %q: %v", cc.openID, err)
+			}
+			if aid != cc.want {
+				t.Errorf("resolve account id: want: %q; got: %q", cc.want, aid)
+			}
+		})
+	}
 }
 
 func TestMain(m *testing.M) {
